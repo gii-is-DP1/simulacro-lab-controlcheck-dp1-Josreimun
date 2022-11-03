44bacc82d31ac5f186fdec264068f1bdff0b22a9
Josreimun <josreimun@alum.us.es>
2022-11-03 15:07:04 +0100

    Completado

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,29 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+    @Query("SELECT p FROM Product p")
     List<Product> findAll();
+
+    @Query("SELECT pt FROM ProductType pt")
     List<ProductType> findAllProductTypes();
+
     Optional<Product> findById(int id);
+
     Product findByName(String name);
+
+    @Query("SELECT pt FROM ProductType pt WHERE pt.name = ?1")
+    public ProductType findTypeByName(String name);
+
+    @Query("SELECT p FROM Product p WHERE p.price < ?1")
+    List<Product> findByPriceLessThan(double price);
+
     Product save(Product p);
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,21 +2,36 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+
     public List<Product> getAllProducts(){
-        return null;
+        return productRepository.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return productRepository.findByPriceLessThan(price);
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return productRepository.findTypeByName(typeName);
     }
 
     public Product save(Product p){
-        return null;       
+        return productRepository.save(p);       
+    }
+
+    public List<ProductType> findAllProductTypes(){
+        return productRepository.findAllProductTypes();
     }
 
     
